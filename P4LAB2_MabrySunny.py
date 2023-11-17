f3b512b261975593aafd4d056aad4bc47f7b7649
sunnymabry <146143832+sunnymabry@users.noreply.github.com>
2023-11-16 19:17:23 -0500

    Add files via upload

---
+++ P4LAB2_MabrySunny.py
@@ -0,0 +1,8 @@
+first = int(input())
+second = int(input())
+if second < first:
+    print("Second integer can't be less than the first.")
+else:
+    for i in range(first, second + 1, 5):
+       print(i, end=" ")
+    print()
